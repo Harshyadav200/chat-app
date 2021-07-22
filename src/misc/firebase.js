549d97381e1efcfbcf1cb7a8872e0e760dad21f2
Harshyadav200 <yadavharshb200@gmail.com>
2021-07-22 17:41:25 +0530

    added sign in page

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyCgH8n6qVBc5oeBQtM_PEgSmOhUWxV9VGI',
+  authDomain: 'chat-web-app-681fc.firebaseapp.com',
+  projectId: 'chat-web-app-681fc',
+  storageBucket: 'chat-web-app-681fc.appspot.com',
+  messagingSenderId: '359932156148',
+  appId: '1:359932156148:web:b3da190d2d52a9e25166a1',
+};
+
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
